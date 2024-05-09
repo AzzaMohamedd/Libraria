0c9ff76161fefdffa81a4eece57b5362c4e59638
Azza <azzagamel11@gmail.com>
2024-05-10 00:18:32 +0300

    finish

---
+++ DAL/VisitorVMRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+using DataModel;
+
+namespace DAL
+{
+    interface IVisitorVMRepository
+    {
+        bool Add(VisitorVM vis);
+    }
+    public class VisitorVMRepository : IVisitorVMRepository
+    {
+        LibraryEntities db = new LibraryEntities();
+        public bool Add(VisitorVM vis)
+        {
+            if (vis != null)
+            {
+                Visitor obj = new Visitor();
+                obj.FirstName = vis.FirstName;
+                obj.LastName = vis.LastName;
+                obj.E_mail = vis.E_mail;
+                obj.Password = vis.Password;
+                db.Visitor.Add(obj);
+                db.SaveChanges();
+                return true;
+            }
+            else
+                return false;
+           
+        }
+    }
+}
---
+++ UI/Controllers/LibraryController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataModel;
+using DAL;
+using BLL;
+
+namespace UI.Controllers
+{
+    public class LibraryController : Controller
+    {
+        LibraryEntities db = new LibraryEntities();
+        VisitorVMRepository rebo = new VisitorVMRepository();
+        
+        // GET: Library
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+       public ActionResult Login()
+        {
+            return View();
+        }     
+        public JsonResult AddAccount(VisitorVM visitor)
+        {
+
+            if (!db.Visitor.Select(x => x.E_mail).Contains(visitor.E_mail))
+            {
+                rebo.Add(visitor);
+                return Json("success", JsonRequestBehavior.AllowGet);
+            }
+            else
+                return Json("error", JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult EnterAccount(VisitorVM visitor)
+        {
+            if (db.Visitor.Select(x => x.E_mail).Contains(visitor.E_mail))
+            {
+                if (db.Visitor.Where(x => x.E_mail == visitor.E_mail && x.Password == visitor.Password).FirstOrDefault() != null)
+                {
+                    return Json("success", JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    //wrong password
+                    return Json("error", JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+                //wrong email
+                return Json("failed", JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
